5e1c102b9b775aecb66e92129923d19749a7ec98
Aurel-flex <aurelien.duberville@gmail.com>
2026-04-30 22:10:34 +0200

    SEO amelioration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,20 @@ import Header from "../components/Header";
 import Breadcrumb from "../components/Breadcrumb"; // 🌟 1. On importe le fil d'Ariane
 import Footer from "../components/Footer";
 import CookieBanner from "../components/CookieBanner";
-
 import { Metadata } from 'next'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: 'Aurélien Duberville | Développeur Web & Éco-conception',
+    default: 'Aurélien Duberville | Webmaster & Éco-conception',
     template: '%s | Aurélien Duberville'
   },
   description: 'Portfolio d\'Aurélien Duberville, expert en développement web durable, Next.js et architecture Headless CMS. Découvrez mes projets et mes articles sur le numérique responsable.',
-  keywords: ['Développeur Web', 'Éco-conception', 'Next.js', 'Portfolio', 'Numérique responsable'],
+  keywords: ['Webmaster', 'Éco-conception', 'Next.js', 'Portfolio', 'Numérique responsable'],
   authors: [{ name: 'Aurélien Duberville' }],
   openGraph: {
-    title: 'Aurélien Duberville | Développeur Web',
-    description: 'Expert en développement web durable et Next.js.',
+    title: 'Aurélien Duberville | Webmaster',
+    description: 'Reponsable de projets web éco-conçus, découvrez mon portfolio et mes articles sur le développement durable.',
     url: 'https://aurelienduberville.fr',
     siteName: 'Aurélien Duberville Portfolio',
     locale: 'fr_FR',
