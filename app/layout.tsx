d3dcb7abb9c9d08d5c7d509463995d762bbcb9f1
Aurel-flex <aurelien.duberville@gmail.com>
2026-05-11 15:00:34 +0200

    SEO url www

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.aurelienduberville.fr'),
   title: {
     default: 'Aurélien Duberville | Webmaster & Intervenant',
     template: '%s | Aurélien Duberville'
